85df2c6f9004ee85a36ed172257b36fd1c64ed88
yuki-kouno <zgoovw@gmail.com>
2021-07-14 00:16:43 +0900

    フロント作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductModule } from './product/product.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), ProductModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      { path: ':productId', component: ProductDetailComponent },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+  ],
+  imports: [RouterModule.forChild(routes), CommonModule],
+  providers: [],
+  bootstrap: [],
+})
+export class ProductModule {}
