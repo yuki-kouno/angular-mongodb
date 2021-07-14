d7297685a6c736fb0663cd0cffc0a5a46c06e315
yuki-kouno <zgoovw@gmail.com>
2021-07-14 14:23:58 +0900

    フロントとDB連携 Fixes #3

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from 'src/app/products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,9 +9,17 @@ import { products } from 'src/app/products';
 export class ProductListComponent implements OnInit {
   products: any;
 
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.products = products;
+    const productsObservable = this.productService.getProducts();
+    productsObservable.subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (err) => {
+        console.error('次のエラーが発生しました:' + err);
+      }
+    );
   }
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product.component';
+import { ProductService } from './shared/product.service';
 
 const routes: Routes = [
   {
@@ -23,7 +24,7 @@ const routes: Routes = [
     ProductDetailComponent,
   ],
   imports: [RouterModule.forChild(routes), CommonModule],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [],
 })
 export class ProductModule {}
