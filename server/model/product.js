039a3d683af2e0870c7bae253fb3aaf176e064e7
yuki-kouno <zgoovw@gmail.com>
2021-07-14 12:05:04 +0900

    node server 作成 Fixes #1

---
+++ server/model/product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// const ObjectId = Schema.ObjectId;
+
+const ProductSchema = new Schema({
+  // author: ObjectBuilder,
+  coverImage: String,
+  name: { type: String, required: true, max: [60, "最大値は60文字までです"] },
+  price: Number,
+  description: String,
+  heading1: String,
+  heading2: String,
+  heading3: String,
+  headingtext1: String,
+  headingtext2: String,
+  headingtext3: String,
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
